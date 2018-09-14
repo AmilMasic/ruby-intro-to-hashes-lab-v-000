5d7788885ebb8c06fcf6902796114949b92fb8c2
Amil Masic <amil.masic@gmail.com>
2018-09-14 23:59:52 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,13 +40,13 @@ def monopoly_with_third_tier
 			:names => {
 				:reading_railroad => {
 					
-				}
+				},
 				:pennsylvania_railroad => {
 					
-				}
+				},
 				:b_and_o_railroad => {
 					
-				}
+				},
 				:shortline => {
 					
 				}
